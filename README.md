99866e8ae9aac267753779852ddab4dfbb68ac04
sabbirofc <mdsabbirhowlader420@gmail.com>
2023-09-12 09:35:11 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Knowledge Cafe
+
+It's an open source repo for all, in this project I create some fake data, and show it using react and vite.
+
+- My Facebook Profile: [Sabbir](https://www.facebook.com/sabbir.offc)
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center my-4 font-semibold text-white">
+        Knowledge Cafe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
